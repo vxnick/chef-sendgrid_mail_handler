5cc230219d7834fb40a6554965fe5368c12646e9
Richard The <richardthe@Richards-MacBook-Pro-SendGrid.local>
2016-03-23 16:10:55 +0700

    only notify failed chef run by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['sendgrid_mail_handler']['to_address'] = 'recipient@domain.com'
 default['sendgrid_mail_handler']['from_address'] = "chef-client@#{node['fqdn']}"
 default['sendgrid_mail_handler']['api_key'] = 'SG.ApiKey'
-default['sendgrid_mail_handler']['send_statuses'] = ['Successful', 'Failed']
+default['sendgrid_mail_handler']['send_statuses'] = ['Failed'] # ['Successful', 'Failed']
 default['sendgrid_mail_handler']['hostname'] = 'localhost'
