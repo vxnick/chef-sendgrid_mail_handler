50f5ad6350a9e80ba04e9c22b379aac8dacfd34e
Richard The <richardthe@Richards-MacBook-Pro-SendGrid.local>
2016-03-23 22:11:59 +0700

    update default recipient email

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['sendgrid_mail_handler']['to_address'] = 'recipient@domain.com'
+default['sendgrid_mail_handler']['to_address'] = 'recipient@example.com'
 default['sendgrid_mail_handler']['from_address'] = "chef-client@#{node['fqdn']}"
 default['sendgrid_mail_handler']['api_key'] = 'SG.ApiKey'
 default['sendgrid_mail_handler']['send_statuses'] = ['Failed'] # ['Successful', 'Failed']
