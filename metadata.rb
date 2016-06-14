7ba87fafc30e367f4c34dc8316a9f5dacc707a60
logikal <sean.kilgore@sendgrid.com>
2016-06-14 13:24:07 -0700

    pin the version of sendgrid-ruby, to keep with a compatible version of ruby bundled with chef

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'operations@sendgrid.com'
 license          'all_rights'
 description      'Installs/Configures SendGrid Mail Handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'chef_handler', '~> 1.3'
